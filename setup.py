b33c348d7957f06e7cba5e094a6cf621a6e534bd
kropchess <chesszn02@gmail.com>
2021-02-15 17:09:10 +0200

    3

--- setup.py
+++ setup.py
@@ -4,19 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="logger-dimitborz",
-    version="0.0.1",
-    author="dimitborz",
-    author_email="dimitborz@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/kropchess/logger.git",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    name='loger',
+    version='0.1',
+    description='Testing installation of Package',
+    url='#',
+    author='auth',
+    author_email='author@email.com',
+    license='MIT',
+    packages=['loger'],
+    zip_safe=False
 )
